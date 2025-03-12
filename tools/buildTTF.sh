60709c4b8fd33a1230848c35de0f984623d3a8bc
Kota Morishita <kouta.morishita@gmail.com>
2025-03-12 15:35:14 -0400

    Build script for TTF to EPD conversion with multiple font sizes

---
+++ tools/buildTTF.sh
@@ -0,0 +1,7 @@
+FONT='../fonts/Poppins-ExtraBold.ttf'
+FONT2='../fonts/Poppins-Regular.ttf'
+python ttfToEPD.py ../fonts/04B.TTF ../weatherCrow5.7/font8.cpp 8
+python ttfToEPD.py $FONT ../weatherCrow5.7/font16.cpp 16
+python ttfToEPD.py $FONT ../weatherCrow5.7/font36.cpp 36
+python ttfToEPD.py $FONT2 ../weatherCrow5.7/font38.cpp 38
+python ttfToEPD.py $FONT ../weatherCrow5.7/font92.cpp 92
